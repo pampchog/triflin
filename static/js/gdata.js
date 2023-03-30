fbaf37a717737fac8c3c3fdbf484edd3b8ab0add
pampchog <129289813+pampchog@users.noreply.github.com>
2023-03-30 07:54:44 +0000

    Update gdata.js

--- static/js/gdata.js
+++ static/js/gdata.js
@@ -12,10 +12,10 @@ var games = [
     'image': 'https://play-lh.googleusercontent.com/rrFtf3hQpmMUmwHdcsvsV6vNpjMP9wsbvt6PpVg6sUleu0iJrlqVQizckc49dNkFQho'
   },
   {
-    'id': 'ekho',
-    'title': '3kh0',
-    'url': 'https://3kh0.github.io',
-    'image': 'https://avatars.githubusercontent.com/u/58097612?v=4'
+    'id': 'spelase',
+    'title': 'Spela.se',
+    'url': 'https://spela.se',
+    'image': 'https://files.cdn.spilcloud.com/drago/1614847196_drago-pointing.png'
   },
   {
     'id': 'a10',
@@ -29,4 +29,46 @@ var games = [
     'url': 'https://poki.se/',
     'image': 'https://a.poki.com/icons/fav-512.png'
   },
+  {
+    'id': '2player',
+    'title': '2 Player Games',
+    'url': 'https://www.twoplayergames.org/',
+    'image': 'https://lh3.googleusercontent.com/WPMDHq8ehWqE4BNS6ORnlQ1J_uFwb7rOduNUykM7EpaTctYwnKq6YenvrpvY4iwKX-ZZ'
+  },
+  {
+    'id': 'spelo',
+    'title': 'Spelo.se',
+    'url': 'https://www.spelo.se/',
+    'image': 'https://tcf.admeen.org/category/1000/846/400x400/ambulance.jpg'
+  },
+  {
+    'id': 'slither',
+    'title': 'Slither.io',
+    'url': 'https://www.slither.io/',
+    'image': 'https://upload.wikimedia.org/wikipedia/en/5/50/Slither.png'
+  },
+  {
+    'id': 'holeio',
+    'title': 'Hole.io',
+    'url': 'https://www.hole-io.com/',
+    'image': 'https://play-lh.googleusercontent.com/YZcHF8esK3O-imazgjP62ZXTWeiW02HpndD4WchscpMIrCe-X8vtDb9o8ut3d7it2CM'
+  },
+  {
+    'id': 'linerider',
+    'title': 'Line rider',
+    'url': 'https://www.linerider.com/',
+    'image': 'https://tcf.admeen.org/category/500/211/400x400/line-rider.jpg'
+  },
+  {
+    'id': 'odd games',
+    'title': 'games.odd.gay',
+    'url': 'https://games.odd.gay/',
+    'image': 'https://games.odd.gay/icons/logo.png'
+  },
+  {
+    'id': 'watchdocs',
+    'title': 'Watchdocumentaries',
+    'url': 'https://watchdocumentaries.com/',
+    'image': 'https://i.pinimg.com/280x280_RS/22/79/cd/2279cd9e2d0809418586c9ecc6e5eb49.jpg'
+  }
 ]
