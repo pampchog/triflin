a232cd322e40dd2d1b662615e9e9bd0c2c2b4301
pampchog <129289813+pampchog@users.noreply.github.com>
2023-04-15 13:24:32 +0200

    Update cloaker.js

--- static/js/cloaker.js
+++ static/js/cloaker.js
@@ -1,38 +1,37 @@
 let inFrame;
 try {
-    inFrame = window !== top
+  inFrame = window !== top;
 } catch (e) {
-    inFrame = !0
-}
-if (!inFrame && !navigator.userAgent.includes("Firefox")) {
-    const e = open("about:blank", "_blank");
-    if (!e || e.closed) alert("Du har popups blockerade! Popups behövs för att hindra denna webbsida från att synas i historiken.\n\nDet borde finnas en knapp eller notis uppe till höger. Tryck där, och tillåt sedan poups från vår sida. Försök sedan komma åt vår sida igen.\n\n Om problemet kvarstår, kontakta oss på vilken sociala medie som helst. "), location.replace("https://google.com");
-    else {
-        const t = e.document,
-            o = t.createElement("iframe"),
-            n = o.style,
-            i = t.createElement("link");
-        t.title = "Ny flik", i.rel = "icon", i.href = "https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_32dp.png", o.src = location.href, n.position = "fixed", n.top = n.bottom = n.left = n.right = 0, n.border = n.outline = "none", n.width = n.height = "100%", t.body.appendChild(o), location.replace("https://google.com")
-    }
+  inFrame = true;
 }
 
-function clockTime() {
-    var e = new Date,
-        t = e.getHours(),
-        o = e.getMinutes(),
-        n = e.getSeconds(),
-        i = "AM";
-    0 == t && (t = 12), t > 12 && (t -= 12, i = "PM");
-    var r = (t = t < 10 ? "0" + t : t) + ":" + (o = o < 10 ? "0" + o : o) + ":" + (n = n < 10 ? "0" + n : n) + " " + i;
-    document.getElementById("MyClock").innerText = r, document.getElementById("MyClock").textContent = r, setTimeout(clockTime, 1e3)
+if (!inFrame && !navigator.userAgent.includes("Firefox")) {
+  const newWindow = open("about:blank", "_blank");
+
+  if (!newWindow || newWindow.closed) {
+    alert("Pop-ups är blockerade.\n\nPop-ups behövs för att gömma denna sida från webbläsarens sökhistorik.\n\nUppe till höger borde det finnas en knapp eller notifikation eller något där du kan acceptera pop-ups för denna sida. Acceptera pop-ups, och gå sedan in på denna sida igen.\n\nOm det inte heller funkar, prata med människan du fick denna sida av, så löser vi problemet.");
+    location.replace("https://google.com");
+  } else {
+    const newDocument = newWindow.document;
+    const newIframe = newDocument.createElement("iframe");
+    const newLink = newDocument.createElement("link");
+
+    newDocument.title = "Google Drive";
+    newLink.rel = "icon";
+    newLink.href = "https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_32dp.png";
+    newIframe.src = location.href;
+    newIframe.style.position = "fixed";
+    newIframe.style.top = "0";
+    newIframe.style.bottom = "0";
+    newIframe.style.left = "0";
+    newIframe.style.right = "0";
+    newIframe.style.border = "none";
+    newIframe.style.outline = "none";
+    newIframe.style.width = "100%";
+    newIframe.style.height = "100%";
+
+    newDocument.head.appendChild(newLink);
+    newDocument.body.appendChild(newIframe);
+    location.replace("https://google.com");
+  }
 }
-clockTime(), document.addEventListener("keypress", (function(e) {
-    if ("Enter" === e.key) {
-        const e = document.getElementById("exploit").value,
-            t = window.open("about:blank", "_blank").document,
-            o = t.createElement("iframe"),
-            n = o.style,
-            i = t.createElement("link");
-        t.title = "Google Drive", o.src = e, n.position = "fixed", n.border = n.outline = "none", n.top = n.bottom = n.left = n.right = 0, n.width = n.height = "100%", i.rel = "icon", i.href = "https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_32dp.png", t.body.appendChild(o)
-    }
-}));
